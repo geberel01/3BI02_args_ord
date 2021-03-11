1b907fd6a90b1ddcb7969374e69f0bfa8e0400ac
Gabriele Barberis Web <alertime1wp@gmail.com>
2021-03-11 07:47:33 +0000

    a

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-#include <stdio.h>
+//ESERCIZIO:
+//DARE IN INGRESSO STRINGHE NUMERICHE
+//E STAMPARE IN ORDINE CRESCENTE E 
+//DECRESCENTE
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int i;
+
+int main(int argc, char *argv[])
+{
+  int n=0, somma=0;
+        for(i=0;i<argc;i++)
+          { 
